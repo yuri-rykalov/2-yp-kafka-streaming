44c78c61d583b9068cfc782a2b894379f9672df5
yuri-rykalov <yurirykalov@yandex.com>
2026-06-18 15:12:47 +0200

    Completed Project 2 - before review

--- sql/ksqldb-queries.sql
+++ sql/ksqldb-queries.sql
@@ -14,27 +14,29 @@ CREATE STREAM messages_stream (
 SELECT * FROM messages_stream EMIT CHANGES ;
 
 -- create table total_users
-CREATE TABLE total_users AS
-WITH (
-    KAFKA_TOPIC = 'table-input-topic',
-    VALUE_FORMAT = 'JSON',
-    KEY = 'user_id'
-);
-
--- create table unique_users
-CREATE TABLE unique_users AS
+CREATE TABLE total_messages AS
 SELECT
     'all' AS grp,
-    COUNT_DISTINCT(user_id) AS unique_users
+    COUNT(*) AS total_messages
 FROM messages_stream
 GROUP BY 'all'
-EMIT CHANGES;
+;
+
+-- create table unique_users
+CREATE TABLE unique_кусшзшутеы AS
+SELECT
+    'all' AS grp,
+    COUNT_DISTINCT(recipient_id) AS unique_recipients
+FROM messages_stream
+GROUP BY 'all'
+;
 
 -- create table user_statistics
 CREATE TABLE user_statistics AS
 SELECT
     user_id,
+    COUNT(*) AS qty_messages,
     COUNT_DISTINCT(recipient_id) AS unique_recipients
 FROM messages_stream
 GROUP BY user_id
-EMIT CHANGES;
+;
